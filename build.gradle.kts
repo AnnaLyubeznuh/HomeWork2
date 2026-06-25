fd704e61e3b2f1b26e6625f8b539a1c91d54766f
annalybeznyh <anna-sherbakova777@yandex.ru>
2026-06-25 23:37:44 +0600

    Lesson_8

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    testImplementation("io.rest-assured:rest-assured:5.4.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
